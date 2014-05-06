de3cfa94d03a1e9ccab1c8361ea766e5e62d4262
Ryan Michael <kerinin@gmail.com>
2014-05-05 21:29:07 -0500

    Agent tests

--- spec/manager/agent_spec.rb
+++ spec/manager/agent_spec.rb
@@ -122,12 +122,94 @@ describe Manager::Agent do
   end
 
   describe "#delete_key" do
+    before(:each) { agent.start }
+
+    it "sends expected request" do
+      stub_request(:delete, "http://localhost/v1/kv/foo")
+      agent.delete_key(:foo)
+      expect(WebMock).to have_requested(:delete, "http://localhost/v1/kv/foo")
+    end
+
+    it "accepts queryargs" do
+      stub_request(:delete, "http://localhost/v1/kv/foo").with(query: {dc: 'dc'})
+      agent.delete_key(:foo) do |b|
+        b.queryargs = {dc: 'dc'}
+      end
+      expect(WebMock).to have_requested(:delete, "http://localhost/v1/kv/foo").with(query: {dc: 'dc'})
+    end
+
+    it "returns true on 200 response" do
+      stub_request(:delete, "http://localhost/v1/kv/foo").
+        to_return(body: 'true')
+
+      expect(agent.delete_key(:foo)).to be_true
+    end
+
+    it "raises exception on non-200 response" do
+      stub_request(:delete, "http://localhost/v1/kv/foo").
+        to_return(status: 500)
+
+      expect { agent.delete_key(:foo) }.to raise_error(Faraday::Error::ClientError)
+    end
   end
 
   describe "#register_check" do
+    before(:each) { agent.start }
+
+    it "sends expected request" do
+      stub_request(:put, "http://localhost/v1/agent/check/register")
+      agent.register_check(:foo)
+      expect(WebMock).to have_requested(:put, "http://localhost/v1/agent/check/register").with(body: :foo)
+    end
+
+    it "accepts queryargs" do
+      stub_request(:put, "http://localhost/v1/agent/check/register").with(query: {dc: 'dc'})
+      agent.register_check(:foo) do |b|
+        b.queryargs = {dc: 'dc'}
+      end
+      expect(WebMock).to have_requested(:put, "http://localhost/v1/agent/check/register").with(query: {dc: 'dc'})
+    end
+
+    it "returns true on 200 response" do
+      stub_request(:put, "http://localhost/v1/agent/check/register")
+
+      expect(agent.register_check(:foo)).to be_true
+    end
+
+    it "raises exception on non-200 response" do
+      stub_request(:put, "http://localhost/v1/agent/check/register").
+        to_return(status: 500)
+
+      expect { agent.register_check(:foo) }.to raise_error(Faraday::Error::ClientError)
+    end
   end
 
   describe "#force_leave" do
+    before(:each) { agent.start }
+
+    it "sends expected request" do
+      stub_request(:get, "http://localhost/v1/agent/force-leave/foo")
+      agent.force_leave(:foo)
+      expect(WebMock).to have_requested(:get, "http://localhost/v1/agent/force-leave/foo")
+    end
+
+    it "accepts queryargs" do
+      stub_request(:get, "http://localhost/v1/agent/force-leave/foo").with(query: {wan: 1})
+      agent.force_leave(:foo) do |b|
+        b.queryargs = {wan: 1}
+      end
+      expect(WebMock).to have_requested(:get, "http://localhost/v1/agent/force-leave/foo").with(query: {wan: 1})
+    end
+
+    it "returns true on 200 response" do
+      stub_request(:get, "http://localhost/v1/agent/force-leave/foo")
+      expect(agent.force_leave(:foo)).to be_true
+    end
+
+    it "raises exception on non-200 response" do
+      stub_request(:get, "http://localhost/v1/agent/force-leave/foo").to_return(status: 500)
+      expect { agent.force_leave(:foo) }.to raise_error(Faraday::Error::ClientError)
+    end
   end
 
   describe "#leave" do
